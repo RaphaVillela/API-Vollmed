57c06c54e6a73f3a7a8a90de890eaaa377809dad
RaphaVillela <raphinha22villela@gmail.com>
2025-08-05 09:39:03 -0300

    Projeto funcionando

---
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,45 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.medico.DadosListagemMedico;
+import med.voll.api.paciente.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteController {
+
+    @Autowired
+    private PacienteRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrarPaciente (@RequestBody @Valid DadosCadastroPaciente dados) {
+
+        repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listarPacientes(@PageableDefault(sort = {"nome"}) Pageable paginacao) {
+        return repository.findAllById(paginacao).map(DadosListagemPaciente::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizarPaciente(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
+        var paciente = repository.getReferenceById(dados.id());
+        paciente.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void removerPaciente(@PathVariable Long id) {
+        var paciente = repository.getReferenceById(id);
+        paciente.inativar();
+    }
+}
