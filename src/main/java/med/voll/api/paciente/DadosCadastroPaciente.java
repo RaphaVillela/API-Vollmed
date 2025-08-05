17a34a7cdfd62fb7c70729835998895fa03eef4a
RaphaVillela <raphinha22villela@gmail.com>
2025-08-05 10:12:03 -0300

    Projeto agora sim, funcionando mesmo

--- src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
@@ -3,6 +3,7 @@ package med.voll.api.paciente;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
@@ -10,5 +11,5 @@ public record DadosCadastroPaciente(@NotBlank String nome,
                                     @NotBlank @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}") String cpf,
                                     @NotBlank @Email String email,
                                     @NotBlank String telefone,
-                                    @NotBlank @Valid DadosEndereco endereco) {
+                                    @NotNull @Valid DadosEndereco endereco) {
 }
