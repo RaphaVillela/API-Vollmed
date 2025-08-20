cc346993090529ef1afd2609dea07e7b855c6a75
RaphaVillela <raphinha22villela@gmail.com>
2025-08-20 14:02:36 -0300

    Agendamento de Consultas adicionado

---
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -0,0 +1,36 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.AgendaDeConsultas;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
+import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/consultas")
+public class ConsultaController {
+
+    @Autowired
+    private AgendaDeConsultas agenda;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity agendarConsulta(DadosAgendamentoConsulta dados) {
+
+        agenda.agendar(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
+    }
+
+    @DeleteMapping
+    @Transactional
+    public ResponseEntity cancelarConsulta(@RequestBody @Valid DadosCancelamentoConsulta dados) {
+        agenda.cancelarConsulta(dados);
+
+        return ResponseEntity.noContent().build();
+    }
+}
